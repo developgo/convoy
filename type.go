289e8eccbf044530caa7b68380249a420f6873e9
ogbanugot <ogbanugot@gmail.com>
2022-03-03 18:52:15 +0100

    update mod, set defaults type.go

--- type.go
+++ type.go
@@ -58,7 +58,7 @@ const (
 	// Maximum number of goroutines fetching messages.
 	MaxNumFetcher = 100
 	// Number of messages reserved by a fetcher in the queue in one request.
-	ReservationSize = 100
+	ReservationSize = 1000
 	//Size of the internal buffer
 	BufferSize = 100000
 )
